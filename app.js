5e50aea9c591ddaf57b5e4fa23689c41833a01d7
Eric Mesquita <eacmesquita@outlook.com>
2021-09-24 16:07:59 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {Storage} = require('@google-cloud/storage');
+
+const app = express();
+const port = process.env.PORT || 8080;
+const bucketName = process.env.BUCKET_NAME;
+//const storage = new Storage({projectId: 'healthy-cubist-326609', keyFile: './service_account.json'});
+const storage = new Storage();
+app.use('/', express.static('dist/angular-client'));
+app.get('/gcsimages', async (req,res) => {
+    const [files] = await storage.bucket(bucketName).getFiles();
+    let images = [];
+    files.forEach(async file => {
+        images.push(file.name);
+      });
+    res.status(200).send(images);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
