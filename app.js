0b075b9c8cb9dbf28a1b72b027413088ebafd161
Eric Mesquita <eacmesquita@outlook.com>
2021-09-25 21:08:10 +0530

    Add components and services

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const bucketName = process.env.BUCKET_NAME;
 //const storage = new Storage({projectId: 'healthy-cubist-326609', keyFile: './service_account.json'});
 const storage = new Storage();
 app.use('/', express.static('dist/angular-client'));
-app.get('/gcsimages', async (req,res) => {
-    const [files] = await storage.bucket(bucketName).getFiles();
-    let images = [];
-    files.forEach(async file => {
-        images.push(file.name);
-      });
+app.get('/cloudimages', async (req,res) => {
+    // const [files] = await storage.bucket(bucketName).getFiles();
+    // let images = [];
+    // files.forEach(async file => {
+    //     images.push(file.name);
+    //   });
+       let images = [{
+          name: 'testimage',
+          publicUrl: 'https://storage.googleapis.com/eric-bucket-test/438.png'
+        }];
     res.status(200).send(images);
 })
 
